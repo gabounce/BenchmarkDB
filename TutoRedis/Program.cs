4995dc7b27a6b13d3cda8198f5992cc09e97b242
gabriel.sagarzazu <gabriel.sagarzazu@onespan.com>
2022-03-31 11:21:33 +0200

    init

---
+++ TutoRedis/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using StackExchange.Redis;
+
+namespace TutoRedis
+{
+    class Program
+    {
+        static readonly ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(
+            new ConfigurationOptions{
+                EndPoints = {"localhost:6379"}                
+            });
+        
+        static async Task Main(string[] args)
+        {
+            BenchmarkRunner.Run<TestRedis>();
+        }
+    }
+
+    public class TestRedis
+    {
+        static readonly ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(
+            new ConfigurationOptions{
+                EndPoints = {"localhost:6379"}                
+            });
+        
+
+        [Benchmark]
+        public void Run()
+        {
+            var db = redis.GetDatabase();
+            var isInList = db.SetContains("users", new RedisValue("67.10.71.45"));
+            if (!isInList)
+            {
+                System.Threading.Thread.Sleep(1000);
+                throw new Exception();
+            }
+        }
+    }
+}
---
+++ WebApplication/Controllers/RedisController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+
+namespace WebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RedisController : ControllerBase
+    {
+        private readonly IConnectionMultiplexer _redis;
+    
+        public RedisController(IConnectionMultiplexer redis)
+        {
+            _redis = redis;
+        }
+
+        [HttpGet("contains/{key}/{val}")]
+        public async Task<IActionResult> Contains(string key, string val)
+        {
+            var db = _redis.GetDatabase();
+            var result = db.SetContains(key, new RedisValue(val));
+
+            return Ok(result);
+        }
+        
+        [HttpGet("scan/{key}/{val}")]
+        public async Task<IActionResult> Scan(string key, string val)
+        {
+            var db = _redis.GetDatabase();
+            var redisValues = db.SetScan(key, new RedisValue(val), 250);
+
+            var results = redisValues.Select(r => r.ToString()).ToArray();
+            return Ok(results);
+        }
+
+        [HttpGet("load/{key}/{n}")]
+        public async Task<IActionResult> Load(string key, int n)
+        {
+            var db = _redis.GetDatabase();
+            db.KeyDelete(key);
+            var rand = new Random();
+            for (var i = 0; i < n; i++)
+            {
+                db.SetAdd(key, new RedisValue(GetRandIp(rand)));
+            }
+
+            return Ok();
+        }
+
+        private static string GetRandIp(Random rand)
+        {
+            return ($"{rand.Next(1, 254)}.{rand.Next(1, 254)}.{rand.Next(1, 254)}.{rand.Next(1, 254)}");
+        }
+    }
+}
